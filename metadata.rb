fe3e88df636adb33ecb55b28438de7cc42583377
Jason Row <jason.row@gmail.com>
2014-04-13 20:53:25 -0300

    eevert "First crack at copying opsworks_delayed_job cookbook in"
    This reverts commit c7ddf8dfbea8390575040aff223da90c9d23c2a5.
    
    Going back to the start

--- metadata.rb
+++ metadata.rb
@@ -5,11 +5,3 @@ version "0.3"
 recipe "opsworks_custom_env::configure", "Write custom configuration and notify rails application to restart upon changes."
 recipe "opsworks_custom_env::restart_command", "Helper recipe that defines command to restart rails application."
 recipe "opsworks_custom_env::write_config", "Write a config/application.yml file to app's deploy directory. Used by configure recipe, or can be used directly in custom situations."
-
-recipe 'opsworks_delayed_job::setup', 'Set up delayed_job worker.'
-recipe 'opsworks_delayed_job::configure', 'Configure delayed_job worker.'
-recipe 'opsworks_delayed_job::deploy', 'Deploy delayed_job worker.'
-recipe 'opsworks_delayed_job::undeploy', 'Undeploy delayed_job worker.'
-recipe 'opsworks_delayed_job::stop', 'Stop delayed_job worker.'
-
-depends 'deploy'
